bee3c73ca4412bd017a7923583aa4eba08539487
Darshana Perera <ds.perera.1997@gmail.com>
2023-05-09 09:14:27 +0530

    web site structure created

---
+++ Dev/vr_shop_react/src/Design/Design.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "./Heading/Heading";
+import Footer from "./Footer/Footer";
+
+export default function Design() {
+  return (
+    <div>
+      <Heading />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
